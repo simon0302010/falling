49c54c489c71e38caa2b52763290e813af0754ca
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-10-29 23:09:42 +0100

    make player head a pumpkin with spooky theme

--- src/game_states.rs
+++ src/game_states.rs
@@ -1,6 +1,7 @@
 use crate::environment::ObstacleObject;
 use crate::player::PlayerData;
 use crate::player_setup::PlayerBodyPart;
+use crate::themes::{Theme, ThemeHandle};
 use bevy::prelude::*;
 
 #[derive(Component)]
@@ -18,7 +19,18 @@ pub enum GameState {
 
 // TODO: better UIs
 
-pub fn spawn_game_over_ui(mut commands: Commands, mut player_data: ResMut<PlayerData>) {
+pub fn spawn_game_over_ui(
+    mut commands: Commands,
+    mut player_data: ResMut<PlayerData>,
+    theme_handle: Res<ThemeHandle>,
+    themes: Res<Assets<Theme>>,
+) {
+    let mut text_color = Color::srgb(1.0, 1.0, 1.0);
+
+    if let Some(theme) = themes.get(&theme_handle.0) {
+        text_color = theme.text_color.to_color();
+    }
+
     commands.spawn((
         Text::new(format!(
             "{}\nPress Space to restart\nScore: {}",
@@ -28,7 +40,7 @@ pub fn spawn_game_over_ui(mut commands: Commands, mut player_data: ResMut<Player
             font_size: 30.0,
             ..Default::default()
         },
-        TextColor(Color::srgb(1.0, 1.0, 1.0)),
+        TextColor(text_color),
         TextLayout::new_with_justify(JustifyText::Center),
         Node {
             position_type: PositionType::Absolute,
@@ -80,6 +92,7 @@ pub fn handle_game_over_input(
     }
 }
 
+// TODO: make pre game ui text color configurable
 pub fn spawn_pre_game_ui(mut commands: Commands) {
     commands.spawn((
         Text::new("Press Space to start."),
--- src/themes.rs
+++ src/themes.rs
@@ -6,6 +6,8 @@ use serde::{Deserialize, Serialize};
 use crate::game_states::GameState;
 use crate::player_setup::PlayerBodyPart;
 
+const THEME_NAME: &str = "spooky";
+
 // defaults for theme
 fn default_black() -> ColorData {
     ColorData {
@@ -60,6 +62,10 @@ fn default_final_color() -> ColorData {
     }
 }
 
+fn default_empty() -> String {
+    "".to_string()
+}
+
 #[derive(Asset, Serialize, Deserialize, Clone, Debug, Reflect)]
 pub struct Theme {
     // color for background
@@ -89,6 +95,9 @@ pub struct Theme {
     // color when broken body part is hit again resulting in the player dying
     #[serde(default = "default_final_color")]
     pub player_final_color: ColorData,
+    // path to image texture of player head. is an empty string by default.
+    #[serde(default = "default_empty")]
+    pub player_head_texture: String,
 }
 
 #[derive(Deserialize, Debug, Clone, Serialize, Reflect)]
@@ -116,7 +125,7 @@ impl ColorData {
 pub struct ThemeHandle(pub Handle<Theme>);
 
 pub fn load_theme(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let theme_handle = ThemeHandle(asset_server.load("themes/default.theme.ron"));
+    let theme_handle = ThemeHandle(asset_server.load(format!("themes/{}.theme.ron", THEME_NAME)));
     commands.insert_resource(theme_handle);
 }
 
@@ -131,6 +140,7 @@ pub fn update_theme(
         With<PlayerBodyPart>,
     >,
     game_state: Res<State<GameState>>,
+    asset_server: Res<AssetServer>,
 ) {
     if !theme_handle.is_changed()
         && !(game_state.is_changed() && *game_state.get() == GameState::InGame)
@@ -153,7 +163,12 @@ pub fn update_theme(
         // player body part color
         for (part_name, mut mesh_material) in player_color_query.iter_mut() {
             if part_name.as_str() == "player_head" {
-                mesh_material.0 = materials.add(theme.player_head_color.to_color());
+                if !&theme.player_head_texture.is_empty() {
+                    mesh_material.0 = materials
+                        .add(asset_server.load(format!("themes/{}", &theme.player_head_texture)))
+                } else {
+                    mesh_material.0 = materials.add(theme.player_head_color.to_color());
+                }
             } else if part_name.as_str().contains("player") {
                 mesh_material.0 = materials.add(theme.player_body_color.to_color());
             }
