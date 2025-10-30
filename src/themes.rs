c55b99fc49da292b615eac2fb0aa231271655fc5
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-10-30 23:42:50 +0100

    made music stop when theme was changed to a theme without music

--- src/themes.rs
+++ src/themes.rs
@@ -4,6 +4,7 @@ use bevy::reflect::Reflect;
 use serde::{Deserialize, Serialize};
 
 use crate::{audio::BackgroundMusic, game_states::GameState};
+use bevy::audio::AudioSink;
 
 #[derive(Resource, Default)]
 pub struct FinishedThemeLoading(pub bool);
@@ -138,19 +139,24 @@ pub fn cycle_theme(
 pub fn update_music(
     themes: Res<Assets<Theme>>,
     theme_handle: Res<ThemeHandle>,
-    mut music_query: Query<&mut AudioPlayer, With<BackgroundMusic>>,
+    mut music_query: Query<(&mut AudioPlayer, Option<&AudioSink>), With<BackgroundMusic>>,
     asset_server: Res<AssetServer>,
     mut just_loaded: ResMut<FinishedThemeLoading>,
 ) {
     if just_loaded.0 {
         if let Some(theme) = themes.get(&theme_handle.0) {
-            if let Ok(mut music_player) = music_query.single_mut() {
+            if let Ok((mut music_player, audio_sink)) = music_query.single_mut() {
                 if !theme.music_path.is_empty() {
                     music_player.0 = asset_server.load(format!("themes/{}", &theme.music_path));
-                    info!("Loaded music.")
+                    if let Some(sink) = audio_sink {
+                        sink.play();
+                    }
+                    info!("Playing background music.")
                 } else {
-                    info!("Stopping music playback.");
-                    music_player.0 = Handle::<AudioSource>::default();
+                    if let Some(sink) = audio_sink {
+                        sink.pause();
+                    }
+                    info!("Stopping background music playback.");
                 }
             }
         }
